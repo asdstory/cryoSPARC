4b0e032a1cafb3630caf8e6ad8ad41a15f67ad9d
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2020-04-26 17:03:39 -0400

    Create worker_config.sh

---
+++ worker_config.sh
@@ -0,0 +1,4 @@
+export CRYOSPARC_LICENSE_ID="133a762e-54e1-11ea-88c5-6b3d93485143"
+export CRYOSPARC_USE_GPU=true
+export CRYOSPARC_CUDA_PATH="/data/jianglab-nfs/programs/lib/cuda-10.1"
+export CRYOSPARC_DEVELOP=false
